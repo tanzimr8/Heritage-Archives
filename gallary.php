9b3ec339a260334a2b95b2b4ff02918eb5d0d924
Tanzim <tanzim008@gmail.com>
2018-05-10 02:49:42 +0600

    TR:Version 1.0

---
+++ gallary.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Gallary- Heritage</title>
+<!-- Bootstrap Core CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+<!-- Custom Fonts -->
+<link href='https://fonts.googleapis.com/css?family=Mrs+Sheppards%7CDosis:300,400,700%7COpen+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800;' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+
+<!-- Plugin CSS -->
+<link rel="stylesheet" href="css/animate.min.css" type="text/css">
+<!-- Custom CSS -->
+
+<link rel="stylesheet" href="css/style.css" type="text/css">
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="page-top">
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+<div class="container">
+	<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand page-scroll" href="index.html"></a>
+		<a class="navbar-brand page-scroll" href="index.html">Heritage</a>
+	</div>
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<ul class="nav navbar-nav navbar-right">
+			<li>
+			<a class="page-scroll" href="index.html">Home</a>
+			</li>
+			<li>
+			<a class="page-scroll" href="#about">Building</a>
+			</li>
+			<li>
+			<a class="page-scroll" href="#collection">Programs</a>
+			</li>
+			<li>
+			<a class="page-scroll" href="#activities">Visitors</a>
+			</li>
+			
+			<li>
+			<a class="page-scroll" href="#contact">Contact</a>
+			</li>
+		</ul>
+	</div>
+	<!-- /.navbar-collapse -->
+</div>
+<!-- /.container -->
+</nav>
+
+
+
+<section id="contact">
+<div class="container">
+<div class="row">
+	<div class="col-lg-12 text-center">
+		<h1 style="color: red; border: 2px solid black">WILL BE UPDATED SOON</h1><br><br>
+		<h2 class="section-heading">CONTACT <b>US</b></h2>
+		<hr class="primary">
+	</div>
+</div>
+<div class="row">	
+	<div class="col-lg-2"></div>
+	<div class="col-lg-8 text-center">
+		<div class="customContact">
+		<div class="contact-item">
+			<i class="fa fa-map-marker "></i>
+          	456-Ka,Kajla, Rajshahi, 6205,Bangladesh<br><br>
+			<i class="fa fa-envelope-o"></i>
+          	mahabub52_heritage@gmail.com<br> 
+          	<i class="fa fa-envelope-o"></i> heritagearchives71@gmail.com <br><br>
+          	<i class="fa fa-phone  "></i> 0721 750801<br>
+          	<i class="fa fa-phone  "></i> +88 01716760485<br>
+        </div>
+        </div>
+	</div>
+	<div class="col-lg-2"></div>	
+</div> <br>
+<div class="row">	
+	<div class="col-md-8 col-md-offset-2 text-center">
+		<div class="regularform">
+			<div class="done">
+				<div class="alert alert-success">
+					<button type="button" class="close" data-dismiss="alert">×</button>
+					Your message has been sent. Thank you!
+				</div>
+			</div>
+			<form method="post" action="contact.php" id="contactform" class="text-left">
+				<input name="name" type="text" class="col-md-6 norightborder" placeholder="Your Name *">
+				<input name="email" type="email" class="col-md-6" placeholder="E-mail address *">
+				<textarea name="comment" class="col-md-12" placeholder="Message *"></textarea>
+				<input type="submit" id="submit" class="contact submit btn btn-primary btn-xl" value="Send message">
+			</form>
+		</div>
+	</div>
+</div>
+</div>
+</section>
+
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/parallax.js"></script>
+<script src="js/contact.js"></script>
+<script src="js/countto.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/common.js"></script>
+</body>
+</html>
